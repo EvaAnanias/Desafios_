ebc6d2cbb0bdfb2032d787d6e53c554bb15043ff
Eva Maria Ananias <75957787+EvaAnanias@users.noreply.github.com>
2025-07-21 16:15:24 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+1. Calculadora Básica:
+Crie uma calculadora que realize as operações básicas: soma, subtração, multiplicação e divisão.
+Permita a entrada de dois números e a escolha da operação.
+Exiba o resultado da operação na tela.
+
+2. Maior e Menor entre Três Números:
+Escreva um programa que peça ao usuário três números.
+Determine o maior e o menor número entre os três e exiba-os na tela.
+
+3. Área de Formas Geométricas:
+Crie um programa que seja capaz de calcular a área de formas geométricas.
+O programa deve permitir o cálculo da área de um quadrado, retângulo, triângulo e círculo.
+O usuário deve escolher a forma geométrica e informar os dados necessários para o cálculo da área.
+
+4. Jogo da Adivinhação:
+Crie um programa que gere um número aleatório entre 1 e 100.
+O usuário deve tentar adivinhar o número com o mínimo de tentativas possível.
+Dê dicas ao usuário, informando se o seu palpite está "acima", "abaixo" ou se ele adivinhou o número.
+
+5. Validador de Senha:
+Escreva um programa que peça ao usuário uma senha.
+A senha deve ter no mínimo 8 caracteres e conter pelo menos uma letra maiúscula, uma letra minúscula e um número.
+Valide a senha e informe ao usuário se ela é válida ou não.
+
+6. Lista de Tarefas:
+Crie um programa que permita ao usuário adicionar, remover e visualizar tarefas.
+Cada tarefa deve ter descrição, prioridade e um status (pendente ou concluída).
+O programa deve permitir a navegação, ordenação e edição da lista de tarefas.
+
+7. Manipulação e Comunicação de Objetos:
+Crie um programa que conecte controles de marcas diferentes (LG, Samsung, Sony, etc) a uma ou mais TVs.
+O programa deve permitir a seleção de um controle e a comunicação com a TV para ligar e desligar;
+As TVs e controles devem ser representados por objetos, e a comunicação entre eles deve ser feita por métodos.
+O programa deve exibir na tela as ações realizadas e o estado atual da TV.
+Caso o controle selecionado não seja compatível com a TV, o programa deve exibir uma mensagem de erro.
+
+8. Estatísticas de Vendas:
+Leia o arquivo em anexo "sales.csv" que contém dados de vendas agrupados por tipo e país.
+Crie um programa que calcule e exiba as seguintes estatísticas:
+Vendas (total de unidades vendidas, total de receita, custo total, lucro total) por tipo de produto.
+Vendas por tipo de produto e região.
+Tipo de produto com maior receita de cada país.
+
+9. Simulador de Financiamento:
+Crie um programa que simule o calculo de financiamento de um imóvel ou veículo, baseado na tabela PRICE.
+O programa deve solicitar o valor total do financiamento, a quantidade de parcelas e a taxa nominal de juros anual.
+O programa deve exibir o valor da parcela, o valor total a ser pago, o custo efetivo total do financiamento e a taxa efetiva mensal.
+O programa deve permitir a simulação de diferentes cenários de financiamento.
+Para este exercício utilize as seguintes fórmulas:
+
+Valor da parcela: PMT = PV * (i / (1 - (1 + i)^-n)), onde PMT é o valor da parcela, PV é o valor total financiado, i é a taxa de juros mensal efetiva e n é a quantidade de parcelas.
+Custo efetivo total: CET = (PMT * n) - PV, onde CET é o custo efetivo total, PMT é o valor da parcela e PV é o valor total financiado.
+Taxa efetiva mensal: im = (1 + i)^(1/12) - 1, onde im é a taxa de juros mensal efetiva e i é a taxa de juros nominal anual.
+
+10. Manipulação de API:
+Utilizando a API REST do GitHub, desenvolva um script que faça uma requisição para obter os repositórios de um usuário e exiba as informações na tela.
+O usuário deve informar o nome do usuário do GitHub.
+O script deve exibir o nome, descrição, linguagem e quantidade de estrelas de cada repositório.
---
+++ desafios.py
@@ -0,0 +1,194 @@
+import random
+import re
+
+try:
+    escolha = int(input("1. Calculadora Básica" +
+                        "\n2. Maior e Menor entre Três Números" +
+                        "\n3. Área de Formas Geométricas" +
+                        "\n4. Jogo da Adivinhação" +
+                        "\n5. Validador de Senha" +
+                        "\n6. Lista de Tarefas" +
+                        "\n7. Manipulação e Comunicação de Objetos" +
+                        "\n8. Estatísticas de Vendas" +
+                        "\n9. Simulador de Financiamento" +
+                        "\n10. Manipulação de API" +
+                        "\nDigite a opção desejada: "))
+
+    if(escolha == 1) :
+        # 1. Calculadora Básica
+
+        print("\nCalculadora\n")
+
+        numero_um = int(input("Digite o primeiro numero para o calculo: "))
+        operador = int(input("Escolha um operador: "+
+                    "\n1. Adicão (+)"+
+                    "\n2. Subtração (-)" +
+                    "\n3. Multiplicação (*)"+
+                    "\n4. Divisão (/)" +
+                    "\n: "))
+        numero_dois = int(input("Digite o segundo numero para o calculo: "))
+
+        if not isinstance(numero_um, int):
+            print("Informação inválida")
+
+        if operador == 1:
+            resultado = numero_um + numero_dois
+            print(f"O resultado da adição é {resultado}")
+
+        elif operador == 2:
+            resultado = numero_um - numero_dois
+            print(f"O resultado da subtração é {resultado}")
+
+        elif operador == 3:
+            resultado = numero_um * numero_dois
+            print(f"O resultado da multiplicação é {resultado}")
+
+        elif operador == 4:
+            resultado = numero_um / numero_dois
+            print(f"O resultado da sua divisão é {resultado}")
+
+        else:
+            print("Operador não encontrado, tente novamente!")
+
+    elif escolha == 2:
+        # 2. Maior e Menor entre Três Números
+
+        print("\nMaior e Menor entre Três Números\n")
+        
+        num_um = int(input("Informe o primeiro número: "))
+        num_dois = int(input("Informe o segundo número: "))
+        num_três = int(input("Informe o terceiro número: "))
+
+        if num_um > num_dois & num_um > num_três:
+            print(f"Número {num_um} é maior que {num_dois} e {num_três}!")
+
+        elif num_dois > num_um & num_dois > num_três:
+            print(f"Número {num_dois} é maior que {num_um} e {num_três}!")
+        
+        else:
+            print(f"Numero {num_três} é maior que {num_um} e {num_dois}")
+
+    elif escolha == 3:
+        # 3. Área de Formas Geométricas
+
+        print("\nÁrea de Formas Geométricas\n")
+
+        escolha = int(input("Escolha uma forma geométrica para calcular a área" +
+                            "\n1. Quadrado" +
+                            "\n2. Retângulo" +
+                            "\n3. Triângulo" +
+                            "\n4. Círculo\n"))
+
+        if escolha == 1:
+
+            q_lado = float(input("Informe o tamanho do quadrado: "))
+
+            area = q_lado * q_lado
+
+            print(f"A área do quadrado é {area}")
+
+        elif escolha == 2:
+
+            comprimento = float(input("Informe a altura do retângulo: "))
+            altura = float(input("Informe a altura do retângulo: "))
+
+            area = comprimento * altura
+
+            print(f"A área do retângulo é {area}")
+
+        elif escolha == 3:
+            
+            base = float(input("Informe o valor da base: "))
+            altura = float(input("Informe a altura do triângulo: "))
+
+            conta_multiplicacao = base * altura
+            area = conta_multiplicacao / 2
+
+            print(f"A área do triângulo é {area}")
+
+        elif escolha == 4:
+            
+            raio = float(input("Informe o raio do circulo: "))
+
+            pi = 3.14
+            area = pi * raio**2
+
+            print(f"A área do circulo é {area}")
+
+    elif escolha == 4:
+        #4. Jogo da Adivinhação
+
+        print("\nJogo da Adivinhação\n")
+
+
+        numero = random.randint(1, 100)
+        tentativas = 1
+        resposta = True
+
+        while resposta:
+
+            valor = int(input("Tente adivinhar o número de 0 a 100: "))
+
+            if valor == numero:
+                print(f"Você acertou! 🎉\nO número era {numero}")
+                resposta = False
+            
+            else:
+                print(f"Poxa você errou! ❌\nO número não é {valor}\n- Você já tentou {tentativas} vez(es).\n")
+                print("Mas você pode tentar novamente!\nVou te dar uma dica beleza?\n")
+
+                tentativas += 1
+
+                if valor > numero:
+                    print("🔻 O número que você digitou é maior que o número a ser adivinhado!\n Boa sorte!\n")
+                
+                else:
+                    print("🔺 O número que você digitou é menor que o número a ser adivinhado!\n Boa sorte!\n")
+    
+    elif escolha == 5:
+        #5. Validador de Senha
+
+        print("\nValidador de Senha\n")
+        
+        resp = True
+
+        while resp:
+            senha = input("Insira a senha: ")
+
+            if len(senha) >= 8:
+                if re.search(r'[A-Z]', senha) and re.search(r'[a-z]', senha):
+                    if any(char.isdigit() for char in senha):
+                        print("Senha Válida")
+                        resp = False
+                    else:
+                        print("Deve haver pelo menos um número na senha.")
+                else:
+                    print("Deve haver ao menos uma letra maiúscula e uma minúscula.")
+            else:
+                print("Senha muito curta, insira uma senha com mais de 8 digitos.") 
+
+    elif escolha == 6:
+        #6. Lista de Tarefas:
+        ...
+
+    elif escolha == 7:
+        #7. Manipulação e Comunicação de Objetos
+        ...
+
+    elif escolha == 8:
+        #8. Estatísticas de Vendas
+        ...
+
+    elif escolha == 9:
+        #9. Simulador de Financiamento
+        ...
+    
+    elif escolha == 10:
+        #10. Manipulação de API
+        ...
+
+    else:
+        print("Informe uma opção válida!")
+        
+except ValueError:
+    print("Por favor, digite apenas números.")
